42aefa9ef97488ae975d3c2c515eaf16eb201c30
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2019-10-23 23:17:54 -0400

    add more descriptive responses for user

--- scripts/startWebScripts.bat
+++ scripts/startWebScripts.bat
@@ -1,6 +1,7 @@
 @echo off
 cd %USERPROFILE%\Documents\Github\LearnDB\scripts\subScripts\
-
+echo "Sarting Change Password Server..."
 START startServer.bat
+echo "Sarting pgweb..."
 START startpgWeb.bat
 PAUSE
