c07244c305f4d67659318ecaa77665a9668935b8
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2019-11-02 14:07:58 -0400

    Add REM statements
    Add name, credit, and licensing information

--- src/pgweb_proxy/exportRules.bat
+++ src/pgweb_proxy/exportRules.bat
@@ -1,4 +1,19 @@
-@echo off
+@ECHO OFF
+REM exportRules.bat - LearnDB
+
+REM Adam Jeniski, Caleb Garrick
+
+REM CC 4.0 BY-NC-SA
+REM https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+REM Copyright (c) 2017- DASSL. ALL RIGHTS RESERVED.
+
+REM ALL ARTIFACTS PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+
+REM Batch file to export inbound and outbound rules from IIS ARR
+REM USAGE: exportRules.bat
+
 echo "Exporting Rules:"
 %windir%/System32/inetsrv/appcmd list config "Default Web Site" -section:system.webServer/rewrite/rules -xml > rules.xml
 echo "Exporting Outbound Rules:"
