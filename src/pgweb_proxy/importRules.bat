b139ae97339f547f9a531b1a26409df16395993f
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2019-11-02 14:10:53 -0400

    Add REM statements

--- src/pgweb_proxy/importRules.bat
+++ src/pgweb_proxy/importRules.bat
@@ -1,4 +1,20 @@
-@echo off
+@ECHO OFF
+REM importRules.bat - LearnDB
+
+REM Adam Jeniski, Caleb Garrick
+
+REM CC 4.0 BY-NC-SA
+REM https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+REM Copyright (c) 2017- DASSL. ALL RIGHTS RESERVED.
+
+REM ALL ARTIFACTS PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+
+REM Batch file to import inbound and outbound rules to IIS ARR from 'rules.xml' and 'outboundRules.xml'
+REM USAGE: importRules.bat
+REM Will read 2 files named 'rules.xml' and 'outboundRules.xml' from the current directory
+
 echo "Importing Rules:"
 %windir%/System32/inetsrv/appcmd set config "Default Web Site" -in < rules.xml
 echo "Importing Outbound Rules:"
